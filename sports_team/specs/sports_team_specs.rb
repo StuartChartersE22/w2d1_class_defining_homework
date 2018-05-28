4ce6ce213cdf3be0df7670e3371caf8a58373d82
StuartCharters <charters.stuart@gmail.com>
2018-05-28 16:41:11 +0100

    refactored for attr_ and tests passed

--- sports_team/specs/sports_team_specs.rb
+++ sports_team/specs/sports_team_specs.rb
@@ -5,23 +5,29 @@ class SportsTeamTest < MiniTest::Test
 
   def test_get_team_name
     new_team = SportsTeam.new("CodeClan")
-    assert_equal("CodeClan",new_team.get_team_name())
+    assert_equal("CodeClan",new_team.team_name())
   end
 
   def test_get_coach
     new_team = SportsTeam.new("CodeClan", "Stuart")
-    assert_equal("Stuart",new_team.get_coach())
+    assert_equal("Stuart",new_team.coach())
   end
 
   def test_get_coach__no_coach
     new_team = SportsTeam.new("CodeClan")
-    assert_nil(new_team.get_coach())
+    assert_nil(new_team.coach())
   end
 
   def test_get_players
     players = ["Bob", "Jay", "Rachel"]
     new_team = SportsTeam.new("CodeClan", "Stuart", players)
-    assert_equal(players,new_team.get_players())
+    assert_equal(players,new_team.players())
+  end
+
+  def test_set_coach
+    new_team = SportsTeam.new("CodeClan")
+    new_team.coach = "Joe"
+    assert_equal("Joe",new_team.coach())
   end
 
 end
--- sports_team/sports_team.rb
+++ sports_team/sports_team.rb
@@ -1,21 +1,12 @@
 class SportsTeam
 
+  attr_accessor(:coach)
+  attr_reader(:team_name, :players)
+
   def initialize(team_name, coach=nil, players=[])
     @team_name = team_name
     @players = players
     @coach = coach
-  end
-
-  def get_team_name()
-    return @team_name
-  end
-
-  def get_coach()
-    return @coach
-  end
-
-  def get_players
-    return @players
   end
 
 end
