6b273a7062ede4032cdf5bc01d6c177d700e02f4
StuartCharters <charters.stuart@gmail.com>
2018-05-28 15:30:23 +0100

    passed getting name from new student

--- codeclan_student/codeclan_student.rb
+++ codeclan_student/codeclan_student.rb
@@ -1,8 +1,14 @@
 class CodeclanStudent
-  
+
   def initialize(student_name, cohort)
     @student_name = student_name
     @cohort = cohort
   end
+
+  def get_name()
+    return @student_name
+  end
+
+  
 
 end
--- codeclan_student/specs/codeclan_student_specs.rb
+++ codeclan_student/specs/codeclan_student_specs.rb
@@ -3,6 +3,8 @@ require_relative("../codeclan_student.rb")
 
 class CodeclanStudentTest < MiniTest::Test
 
-  p CodeclanStudent.new("Stuart", "E22")
-
+  def test_creating_new_student__return_name
+    new_student = CodeclanStudent.new("Stuart", "E22")
+    assert_equal("Stuart", new_student.get_name())
+  end
 end
